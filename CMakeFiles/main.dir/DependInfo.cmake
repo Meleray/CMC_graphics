965a9725322e9a2526a0b27fa8eeaf6e249e1565
Meleray <konovma@gmail.com>
2021-02-19 12:04:21 +0300

    Added HW1 cpp template

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/template1_cpp/glad.c" "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/CMakeFiles/main.dir/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "template1_cpp/dependencies/include/GLAD"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/template1_cpp/Image.cpp" "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/CMakeFiles/main.dir/Image.cpp.o"
+  "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/template1_cpp/Player.cpp" "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/CMakeFiles/main.dir/Player.cpp.o"
+  "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/template1_cpp/main.cpp" "/home/mk/Documents/CMC/SEM_6/Graphics/msu_cmc_cg_2021/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "template1_cpp/dependencies/include/GLAD"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
